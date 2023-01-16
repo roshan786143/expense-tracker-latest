b5175357efebe9f51e63d281eb7eef31e122f9bd
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-16 16:08:22 +0530

    decreypted the user password at login and verify with the database

--- controllers/login.js
+++ controllers/login.js
@@ -2,12 +2,10 @@ const sequelize = require('../util/database');
 const userLoginDetails = require('../models/userLoginDetails');
 const bcrypt = require('bcrypt');
 
-const login = async (req, res) => {
+const login = (req, res) => {
   let credentials = req.body;
 
   const { email, password } = credentials;
-
-  // let decryptedPassword = await bcrypt.compare---------------------------------------->
 
   userLoginDetails
     .findOne({
@@ -15,15 +13,33 @@ const login = async (req, res) => {
         email: email,
       },
     })
-    .then((response) => {
-      if (response !== null) {
-        if (response.dataValues.password == credentials.password) {
-          res.send('login successful.');
-        } else {
-          res.send(
-            'user exist but password do not match,pls.check your password and come back.'
-          );
-        }
+    .then((record) => {
+      if (record !== null) {
+
+        bcrypt.compare(credentials.password,record.dataValues.password)
+        .then((check)=>{
+            console.log("my normal password -->",credentials.password);
+            console.log("my encrypted password -->",record.dataValues.password);
+            console.log("my record -->",record);
+            console.log("decrypt -->",check);
+
+            if(check){
+                res.send('login successful.')
+            }else{
+                res.send('user exist but password do not match,pls.check your password and come back.')
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+
+        // if (response.dataValues.password == password) {
+        //   res.send('login successful.');
+        // } else {
+        //   res.send(
+        //     'user exist but password do not match,pls.check your password and come back.'
+        //   );
+        // }
       } else {
         res.send("user doesn't exist.");
       }
